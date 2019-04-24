2a3b806537568b49a4d71dccdbab7ba65eeb40ee
PhilippeHage <48258294+PhilippeHage@users.noreply.github.com>
2019-04-24 16:44:07 +0200

    Create test_sum2
    I added the filtering for the data type

---
+++ tests/test_sum2.py
@@ -0,0 +1,30 @@
+# importing necessary packages numpy and statistics
+import numpy as np
+import statistics
+
+# import the created dataframe class
+from ie_pandas import DataFrame
+
+# simple test to get the sum of the values
+def test_sum1():
+
+    dictionary = {
+        "pet": np.array(["cat", "dog", "mouse"]),
+        "age": np.array([1, 2, 3]),
+        "weight": np.array([1.0, 2.0, 3.0]),
+        "sick": np.array([True, True, False]),
+    }
+
+    df = DataFrame(dictionary)
+    sum_collector = []
+    columns = []
+    for key in dictionary:
+        if dictionary[key].dtype == "float64" or dictionary[key].dtype == "int32":
+            columns.append(key)
+            sum_collector.append(sum(dictionary[key]))
+
+    expected_output = sum_collector
+
+    output = df.sum
+
+    assert output == expected_output
