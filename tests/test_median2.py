08e576b93a042f28f2c1a7e009dd7a75b629350b
PhilippeHage <48258294+PhilippeHage@users.noreply.github.com>
2019-04-24 17:54:31 +0200

    Create test_median2.py

---
+++ tests/test_median2.py
@@ -0,0 +1,30 @@
+# importing necessary packages numpy and statistics
+import numpy as np
+import statistics
+
+# import the created dataframe class
+from ie_pandas import DataFrame
+
+# simple test to get the median of the values
+def test_median2():
+
+    dictionary = {
+        "pet": np.array(["cat", "dog", "mouse"]),
+        "age": np.array([1, 2, 3]),
+        "weight": np.array([1.0, 2.0, 3.0]),
+        "sick": np.array([True, True, False]),
+    }
+
+    df = DataFrame(dictionary)
+    median_collector = []
+    columns = []
+    for key in dictionary:
+        if dictionary[key].dtype == "float64" or dictionary[key].dtype == "int32":
+            columns.append(key)
+            median_collector.append(median(dictionary[key]))
+
+    expected_output = median_collector
+
+    output = df.median
+
+    assert output == expected_output
