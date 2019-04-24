13bc831ce493b1d33554aceb3c74ca000b18fbe2
dominiktha <44036713+dominiktha@users.noreply.github.com>
2019-04-24 16:57:00 +0200

    adding test_ncols

---
+++ tests/test_ncols.py
@@ -0,0 +1,24 @@
+# importing necessary packages numpy and statistics
+import numpy as np
+import statistics
+
+# import the created dataframe class
+from ie_pandas import DataFrame
+
+# simple test to get the number of columns
+def test_get_number_of_columns():
+
+    dictionary = {
+        "pet": np.array(["cat", "dog", "mouse"]),
+        "age": np.array([1, 2, 3]),
+        "weight": np.array([1.0, 2.0, 3.0]),
+        "sick": np.array([True, True, False]),
+    }
+
+    df = DataFrame(dictionary)
+
+    expected_output = len(dictionary) 
+
+    output = df.ncols
+
+    assert output == expected_output
