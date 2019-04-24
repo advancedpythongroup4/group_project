16623f33772ce6383365ea886bf32f8f5490a4fd
Marcos <marcos.berges@gmail.com>
2019-04-24 16:41:00 +0200

    add 2 test: create and get

--- tests/test_create_DataFrame.py
+++ tests/test_create_DataFrame.py
@@ -1,1 +1,12 @@
 # test whether creating a DataFrame works.
+from ie_pandas import DataFrame
+import numpy as np
+def test_create_dataframe():
+    
+    dictionary = {
+    "pet": np.array(["cat", "dog", "mouse"]),
+    "age": np.array([1, 2, 3]),
+    "weight": np.array([1.0, 2.0, 3.0]),
+    "sick": np.array([True, True, False]),
+    }
+    df=DataFrame(dictionary)
