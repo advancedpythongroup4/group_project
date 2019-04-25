e55da4f2a14a2c487c56d2c940359ca8f6542be2
Marcos <marcos.berges@gmail.com>
2019-04-25 14:11:28 +0200

    add setter test

---
+++ tests/test_set.py
@@ -0,0 +1,24 @@
+# Test Set
+import numpy as np
+from ie_pandas import DataFrame
+def test_set():
+    dictionary = {
+        "pet": np.array(["cat", "dog", "mouse"]),
+        "age": np.array([1, 2, 3]),
+        "weight": np.array([1.0, 2.0, 3.0]),
+        "sick": np.array([True, True, False]),
+    }
+    df=DataFrame(dictionary)
+    
+    df["pet"] = np.array(["frog", "lizard", "turtle"])
+
+
+    pet0="frog"
+    pet1="lizard"
+    pet2="turtle"
+
+    assert df["pet"][0]==pet0
+    assert df["pet"][1]==pet1
+    assert df["pet"][2]==pet2
+
+
