0671da1acf3f868e4a9a1edf658d9b67447108fa
dominiktha <44036713+dominiktha@users.noreply.github.com>
2019-05-05 23:12:38 +0200

    blacking

--- tests/test_sum2.py
+++ tests/test_sum2.py
@@ -20,8 +20,7 @@ def test_sum2():
     sum_collector = []
     columns = []
     for key in dictionary:
-        if dictionary[key].dtype == "float64" or 
-            dictionary[key].dtype == "int32":
+        if dictionary[key].dtype == "float64" or dictionary[key].dtype == "int32":
             columns.append(key)
             sum_collector.append(sum(dictionary[key]))
 
